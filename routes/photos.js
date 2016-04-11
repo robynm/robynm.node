18d0f130c76ee40404cf81f73eda70a27b8df067
Robyn Meshulam <robyn.meshulam@gmail.com>
2016-04-11 15:57:58 -0700

    some styling of photos

--- routes/photos.js
+++ routes/photos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Photos = require('../models/photos');
 
 router.get('/', function(req, res, next) {
-  var p = new Photos('flickr.people.getPublicPhotos', 6, 1);
+  var p = new Photos('flickr.people.getPublicPhotos', 12, 1);
   var data = p.getPhotos(function(response) {
     var d = JSON.parse(response);
     res.render('photos', {
