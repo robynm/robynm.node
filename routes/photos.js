0cb296650463e6f1226927fa65d592ac05c8daaa
Robyn Meshulam <robyn.meshulam@gmail.com>
2016-04-12 19:25:23 -0700

    Adding some more modules and configuring build and watch tasks for npm

--- routes/photos.js
+++ routes/photos.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var Photos = require('../models/photos');
 
 router.get('/', function(req, res, next) {
-  var p = new Photos('flickr.people.getPublicPhotos', 12, 1);
-  var data = p.getPhotos(function(response) {
-    var d = JSON.parse(response);
+  // var p = new Photos('flickr.people.getPublicPhotos', 12, 1);
+  // p.getPhotos(function(response) {
+    // var d = JSON.parse(response);
     res.render('photos', {
-      pics: d.photos.photo, 
+      // pics: d.photos.photo, 
       title: 'robyn meshulam', 
       page_title: 'photos'
     });
-  });
+  // });
 });
 
 module.exports = router;
